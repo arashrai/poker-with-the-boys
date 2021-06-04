9bd1aef10f1a408874be646fe3440d0594c30ad4
arashrai <arashrai17@gmail.com>
2021-06-03 19:09:32 -0700

    rip last hand

--- graph_night.py
+++ graph_night.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime
 
-CSV_FILE = "poker_night_20210527.csv"
+CSV_FILE = "poker_night_20210603.csv"
 logs = []
 
 with open(CSV_FILE) as file:
