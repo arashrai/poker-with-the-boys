95cfee9419be296084353d2d19f8707834f5caf1
arashrai <arashrai17@gmail.com>
2021-07-08 19:07:31 -0700

    hopefully the stock to poker relationship holds

--- graph_night.py
+++ graph_night.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime
 
-CSV_FILE = "poker_night_20210701.csv"
+CSV_FILE = "poker_night_20210708.csv"
 logs = []
 
 with open(CSV_FILE) as file:
