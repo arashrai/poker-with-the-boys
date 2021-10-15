f7bc3b3fea3d02740d1df1939fa08ad14393eefa
arashrai <arashrai17@gmail.com>
2021-10-15 09:48:36 -0700

    peelic makes 20 cents

--- graph_night.py
+++ graph_night.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime
 
-CSV_FILE = "poker_night_20211003.csv"
+CSV_FILE = "poker_night_20211014.csv"
 logs = []
 
 with open(CSV_FILE) as file:
