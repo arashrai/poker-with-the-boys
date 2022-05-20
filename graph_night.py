e93d136c7d1fba83679ac5cfb7f8b5a4bc9c940e
arashrai <arashrai17@gmail.com>
2022-05-19 19:05:57 -0700

    big spenny night pt 2

--- graph_night.py
+++ graph_night.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime
 
-CSV_FILE = "poker_night_20220512.csv"
+CSV_FILE = "poker_night_20220519.csv"
 logs = []
 
 with open(CSV_FILE) as file:
