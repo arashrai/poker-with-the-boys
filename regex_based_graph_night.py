1b6e217aeee2c948c4917dfe860557ce62bb303e
Stephen Melinyshyn <stephen.melinyshyn@gmail.com>
2023-03-16 20:34:29 -0700

    winning hand types per player

--- regex_based_graph_night.py
+++ regex_based_graph_night.py
@@ -63,7 +63,7 @@ KNOWN_NAME_FIX_UPS = {
         "max": "Max",
         "sam": "Sam",
         "daveeeeeed": "David",
-        "stevo-tesla": "Stephen",        
+        "stevo-tesla": "Stephen",
 }
 
 class RoundAction(Enum):
@@ -82,7 +82,7 @@ class PlayerRoundAction():
         self.amount = int(amount)
         self.action_type = round_action
         self.time = datetime.fromtimestamp(int(unix_time) / 100000)
-        
+
     def to_string(self):
         if self.amount > 0:
             return f'{self.player} {self.action_type.value} {self.amount} at {self.time}'
@@ -320,18 +320,18 @@ class PokerRound(): # multiple rounds in a poker night event
             elif re.match(PLAYER_RAISES_REGEX, row):
                 player, amount, unix_time = re.findall(PLAYER_RAISES_REGEX, row)[0]
                 round_action = PlayerRoundAction(player, RoundAction.raises, unix_time, amount)
-    
+
             if round_action:
                 is_all_in = "go all in" in row
                 round_action.all_in = is_all_in
                 player_actions.append(round_action)
-        
+
         self.player_actions = player_actions
-        
+
     def pre_flop_actions(self):
         if not hasattr(self, 'flop_time'): # sometimes rounds don't go to a flop
             return self.player_actions
-            
+
         return list(filter(lambda action: action.time < self.flop_time, self.player_actions))
 
     def pre_turn_actions(self):
@@ -347,17 +347,17 @@ class PokerRound(): # multiple rounds in a poker night event
     def pre_river_actions(self):
         if not hasattr(self, 'turn_time'): # sometimes rounds don't go to a turn
             return []
-    
+
         # if the round ends after the turn, but before the river, get all the actions after turn
         if not hasattr(self, 'river_time'):
             return list(filter(lambda action: action.time >= self.turn_time, self.player_actions))
 
         return list(filter(lambda action: action.time >= self.turn_time and action.time < self.river_time, self.player_actions))
-    
+
     def post_river_actions(self):
         if not hasattr(self, 'river_time'): # sometimes rounds don't go to a turn or river
             return []
-        
+
         return list(filter(lambda action: action.time >= self.river_time, self.player_actions))
 
 def date_of_csv(csv_name):
@@ -379,7 +379,7 @@ def fix_up_player_names(log_lines):
                     print("Not sure if this person is already normalized", name)
                     print("If this is a new player, add them to KNOWN_NAME_FIX_UPS and run again")
                     exit(-1)
-                
+
                 existing_player_name_format= '""%s @ ' % name
                 replacement_player_name_format = '""%s @ ' % normalized_name
                 line = line.replace(existing_player_name_format, replacement_player_name_format)
@@ -448,45 +448,45 @@ def biggest_win(rounds):
             if amount > biggest_amount:
                 biggest_amount = amount
                 biggest_round = round
-                
+
     return biggest_round
-    
+
 # returns the number of rounds where a player was seated
 def rounds_played_by_players(rounds):
     player_round_counts = defaultdict(lambda: 0)
     for round in rounds:
         for player in round.player_balances: # they only have a balance if they're in the start of that round
             player_round_counts[player] += 1
-        
+
     return player_round_counts
-    
+
 # returns the largest raise/bet for the round actions provided
 def largest_raise_or_bet_for_round_actions(round_actions):
     biggest_raise_action = None
-    
+
     raise_or_bet_actions = filter(lambda action: action.action_type in [RoundAction.raises, RoundAction.bets], round_actions)
     for raise_action in raise_or_bet_actions:
         if biggest_raise_action is None or raise_action.amount > biggest_raise_action.amount:
                 biggest_raise_action = raise_action
-    
+
     return biggest_raise_action
-    
+
 # returns the number of folds per player in round_actions
 def number_of_folds_per_player(round_actions):
     player_folds = defaultdict(lambda: 0)
     fold_actions = filter(lambda action: action.action_type == RoundAction.folds, round_actions)
     for action in fold_actions:
         player_folds[action.player] += 1
-        
+
     return player_folds
-    
+
 # all-ins for players in round_actions
 def all_ins_per_player(round_actions):
     player_all_ins = defaultdict(lambda: [])
     for round in round_actions:
         if round.all_in:
             player_all_ins[round.player].append(round)
-            
+
     return player_all_ins
 
 # returns the winning PokerRounds by each player corresponding to the rounds in the
@@ -498,9 +498,9 @@ def player_wins_for_round_actions(all_rounds, player_to_round_actions):
             poker_round = poker_round_for_timestamp(all_rounds, action.time) # use the timestamp to look up the round (inefficient, but it works)
             if player in poker_round.winning_players:
                 player_to_winning_rounds[player].append(poker_round)
-        
+
     return player_to_winning_rounds
- 
+
 # returns the number of times per player that they showed their hidden hand
 # after they won the round
 def gentleman_scores_by_player(rounds):
@@ -513,11 +513,38 @@ def gentleman_scores_by_player(rounds):
 
     return player_to_num_hand_shows_post_win
 
+# returns the number of different types of shown winning hands per player
+def winning_hand_types_by_player(rounds):
+    player_to_hand_type_to_wins = defaultdict(lambda: defaultdict(lambda: 0))
+    hands_with_known_win = 0
+    for round in rounds:
+        for i in range(len(round.winning_players)):
+            if round.winning_hands == []:
+                continue # unknown winning hand
+            winning_player = round.winning_players[i]
+            hand = round.winning_hands[i]
+            hands_with_known_win += 1
+            prefix, _ = hand.split("(combination:")# i.e. Pair, K's
+            hand_type = prefix.split(",")[0] # i.e. Pair
+            if "High" in hand_type: # "A's High" -> "High Card"
+                hand_type = "High Card"
+            player_to_hand_type_to_wins[winning_player][hand_type] += 1
+
+    hand_display_sort_order = {"High Card": 0, "Pair": 1, "Two Pair": 2, "Three of a Kind": 3, "Straight": 4, "Flush": 5, "Full House": 6, "Four of a Kind": 7, "Straight Flush": 8}
+    for player, winning_hands_counts in player_to_hand_type_to_wins.items():
+        sorted_winning_hands_counts = sorted(winning_hands_counts.items(), key=lambda i:hand_display_sort_order[i[0]])
+        print(f'{player}:')
+        for hand, count in sorted_winning_hands_counts:
+            print(f'  {hand}: {count}/{hands_with_known_win} ({count / hands_with_known_win * 100.0:.2f}%)')
+
+    return player_to_hand_type_to_wins, hands_with_known_win
+
+
 
 def print_core_stats(rounds):
     num_rounds_with_player = rounds_played_by_players(rounds)
     most_wins_player, winning_rounds, all_player_wins = most_wins(rounds)
-    
+
     all_player_wins_list = list(all_player_wins.items())
     all_player_wins_list.sort(key=lambda w: len(w[1]), reverse=True)
     print("\n------- Winning Hands of Hands Played")
@@ -528,11 +555,11 @@ def print_core_stats(rounds):
     biggest_win_round = biggest_win(rounds)
     print("\n------- Biggest Winning Hand")
     print(f'{", ".join(biggest_win_round.winning_players)} won the most at {biggest_win_round.winning_amounts} on {biggest_win_round.start_time}.\nTable cards: {biggest_win_round.table_cards}.\nWinning hands: {", ".join(biggest_win_round.winning_hands)}.\nAll player\'s cards: {biggest_win_round.player_to_hand}')
-    
+
     gent_scores_by_player_dict = gentleman_scores_by_player(rounds)
     gent_scores_by_player = list(gent_scores_by_player_dict.items())
     gent_scores_by_player.sort(key=lambda p: p[1], reverse=True)
-    
+
     print("\n------- Gentleman Scores (Showing Hidden Hand After Win)")
     formatted = "\n".join("{: >10} {: >5} ({:,.2f}%)".format(player, gent_score, gent_score / len(all_player_wins[player]) * 100.0) for player, gent_score in gent_scores_by_player)
     print(formatted)
@@ -541,12 +568,12 @@ def print_core_stats(rounds):
     all_pre_turn_actions = reduce(lambda acc_actions, round: acc_actions + round.pre_turn_actions(), rounds, [])
     all_pre_river_actions = reduce(lambda acc_actions, round: acc_actions + round.pre_river_actions(), rounds, [])
     all_post_river_actions = reduce(lambda acc_actions, round: acc_actions + round.post_river_actions(), rounds, [])
-    
+
     biggest_raise_pre_flop = largest_raise_or_bet_for_round_actions(all_pre_flop_actions)
     biggest_raise_pre_turn = largest_raise_or_bet_for_round_actions(all_pre_turn_actions)
     biggest_raise_pre_river = largest_raise_or_bet_for_round_actions(all_pre_river_actions)
     biggest_raise_post_river = largest_raise_or_bet_for_round_actions(all_post_river_actions)
-    
+
     print("\n------- Biggest Raises/Bets")
     # if biggest_raise_pre_flop:
     #     print("--- Pre-flop")
@@ -568,10 +595,10 @@ def print_core_stats(rounds):
         print(f'{biggest_raise_post_river.to_string()}')
         round = poker_round_for_timestamp(rounds, biggest_raise_post_river.time)
         print(f'  {", ".join(round.winning_players)} won {round.winning_amounts} this round\n  Table cards: {round.table_cards}\n  Winning hands: {", ".join(round.winning_hands)}\n  All player\'s cards: {round.player_to_hand}')
-        
+
     player_pre_flop_folds = list(number_of_folds_per_player(all_pre_flop_actions).items())
     player_pre_flop_folds.sort(key=lambda w: w[1], reverse=True)
-    
+
     player_pre_turn_folds = list(number_of_folds_per_player(all_pre_turn_actions).items())
     player_pre_turn_folds.sort(key=lambda w: w[1], reverse=True)
 
@@ -599,7 +626,7 @@ def print_core_stats(rounds):
     player_pre_flop_all_ins = list(player_pre_flop_all_ins_dict.items())
     player_pre_flop_all_ins.sort(key=lambda w: len(w[1]), reverse=True)
     player_wins_for_pre_flop_all_ins = player_wins_for_round_actions(rounds, player_pre_flop_all_ins_dict)
-    
+
     player_pre_turn_all_ins_dict = all_ins_per_player(all_pre_turn_actions)
     player_pre_turn_all_ins = list(player_pre_turn_all_ins_dict.items())
     player_pre_turn_all_ins.sort(key=lambda w: len(w[1]), reverse=True)
@@ -634,6 +661,9 @@ def print_core_stats(rounds):
         print("--- Post-river")
         formatted = "\n".join("{: >10} {: >10} {:,.2f}% (Won {:,.2f}%)".format(player, len(all_ins), len(all_ins) / num_rounds_with_player[player] * 100.0, len(player_wins_for_post_river_all_ins[player]) / len(all_ins) * 100.0) for player, all_ins in player_post_river_all_ins)
         print(formatted)
+
+    print("\n------- Winning Hand Breakdown By Player")
+    winning_hand_types_by_player(rounds)
 
 ### Main execution
 
@@ -694,6 +724,6 @@ else:
 
         # Print some stats out
         print_core_stats(event.rounds)
-        
+
         graph_stack_history(player_history, "Profit for " + event_date, csv_file)
 
